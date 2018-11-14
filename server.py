5ee79f22e80ec7d0fb2cd8099d237f1e334d3c4f
Kyle Van Landingham <kylevanlandingham9@gmail.com>
2018-11-13 19:05:05 -0800

    Added status checking

--- server.py
+++ server.py
@@ -4,15 +4,29 @@ import threading
 
 app = Flask(__name__)
 
+running = False
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/algorithm', methods=['POST'])
 def algorithm():
-    data = request.get_json();
-    threading.Thread(target=process(data)).start()
-    return "";
+    global running
+    if (running == False):
+        data = request.get_json();
+        threading.Thread(target=process(data)).start()
+    return ""
+
+@app.route('/status', methods=['GET'])
+def checkStatus():
+    if (running == False):
+        return "false"
+    else:
+        return "true"
 
 def process(data):
-    MinCostMaxFlow.flow(data["students"], data["projects"], data["projMinCapacity"], data["projMaxCapacity"], data["ranking"])
+    global running
+    running = True
+    MinCostMaxFlow.flow(data["students"], data["projects"], data["projMinCapacity"], data["projMaxCapacity"], data["rankings"])
+    running = False
